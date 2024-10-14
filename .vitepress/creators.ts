367f6459795be0667a1ec66f761d71fbe0c45de7
Misaka007 <54129287+Misaka007@users.noreply.github.com>
2024-10-14 21:47:22 +0800

    更新 creators.ts

--- .vitepress/creators.ts
+++ .vitepress/creators.ts
@@ -26,7 +26,7 @@ export const creators: Creator[] = [
     title: 'Wisdom Workshop 原始创作者',
     desc: '开发者，专注于基础设施维护，数据分析，后端、DevOps 开发',
     links: [
-      { type: 'github', icon: 'github', link: 'https://github.com/nekomeowww' },
+      { type: 'github', icon: 'github', link: 'https://github.com/Misaka007' },
       { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/ayakaneko' },
     ],
     nameAliases: ['nekomeowww', '绚香猫', '絢香猫', 'Neko Ayaka', 'Ayaka Neko'],
@@ -39,7 +39,7 @@ export const creators: Creator[] = [
     title: 'Wisdom Workshop 原始创作者',
     desc: '开源开发者，专注于前端，以及前端相关工具库和工具链开发',
     links: [
-      { type: 'github', icon: 'github', link: 'https://github.com/LittleSound' },
+      { type: 'github', icon: 'github', link: 'https://github.com/Misaka007' },
       { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/OikawaRizumu' },
     ],
     nameAliases: ['LittleSound', '绚香音', '絢香音', 'Rizumu Oikawa', 'Rizumu Ayaka', 'Ayaka Rizumu', 'Rizumu'],
